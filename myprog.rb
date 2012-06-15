8e0126f67492a50c8c5474e6bf9aac3ac6db3738
Mark Moschel <mark.moschel@gmail.com>
2012-06-15 15:02:59 -0400

    small changes

--- myprog.rb
+++ myprog.rb
@@ -1,17 +1,12 @@
-#!/usr/bin/env ruby
-require 'date' 
+puts "A"
+require 'rubygems'
+puts "B"
+puts ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
 
-#str = (STDIN.tty?) ? 'not reading from stdin' : $stdin.read
-
-str = "@p1 asdf ads ds #3 task1, task 2 fads ds a dfa  dsf a
-@p2 #1 task4, task 5
-fas @p3 to #1 fasdfla sf
-fasfadsfa"
-
-regex_entries = /@([a-zA-Z0-9]+).*#(\d+\.?\d*)(.*)/
-str.scan(regex_entries) { |r|
-	puts r[0]
-	puts r[1]
-	puts r[2].strip
-
-}
+puts "C"
+puts "FILE" if File.exists?(ENV['BUNDLE_GEMFILE'])
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+puts "With env: #{ENV['BUNDLE_GEMFILE']}"
+Bundler.with_clean_env do
+  puts "With clean: #{ENV['BUNDLE_GEMFILE']}"
+end
