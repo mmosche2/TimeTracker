a61751835b0add382d6ab87e5408aaf6f170e284
Mark Moschel <mark.moschel@gmail.com>
2012-06-18 20:20:25 -0400

    email url changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,11 @@ class UserMailer < ActionMailer::Base
   
   def welcome_email(user)
 	@user = user
-	@url = "http://localhost:3000"
 	mail(:to => user.email, :subject => "Welcome to Experimentable!")
   end
   
   def daily_email(user)
 	@user = user
-	@url = "http://localhost:3000"
 	mail(:to => user.email, :subject => "What did you do today?")
   end
 	
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -8,5 +8,3 @@ ActionMailer::Base.smtp_settings = {
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
-
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
