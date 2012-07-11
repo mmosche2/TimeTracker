437131e5e472d9d405bc6183a53346b7a1edc7f0
Mark <mark.moschel@gmail.com>
2012-07-11 18:00:37 -0500

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Journal::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "www.timefeather.com" }
 
   # Enable threaded mode
   # config.threadsafe!
